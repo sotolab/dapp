d0bc62769a196f8fceff6532a8dfcb01a36d822d
sotojang <mrjang28@gmail.com>
2018-07-15 20:03:15 +0900

    first commit

---
+++ client/config.js
@@ -0,0 +1,10 @@
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+    /*
+    extraSignupFields: [{
+        fieldName: "username",
+        fieldLabel: "username",
+        inputType: 'text'
+    }]
+    */
+  });
